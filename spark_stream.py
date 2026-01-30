0a8deb2f7318336143df41c620b207f4d0abd7d7
saiharithalp07 <saiharithalp07@gmail.com>
2026-01-29 20:41:08 -0600

    imported numpy

--- spark_stream.py
+++ spark_stream.py
@@ -4,7 +4,7 @@ from cassandra.cluster import Cluster
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import StructType, StructField, StringType
-
+import numpy
 
 def create_keyspace(session):
     session.execute("""
